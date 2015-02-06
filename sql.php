b1dd4b17487d232836a29c5a712afc212df442b7
dbursem <david@dbursem.nl>
2015-02-06 13:54:02 +0100

    put back old mysql_connect() function for legacy support

--- sql.php
+++ sql.php
@@ -9,3 +9,21 @@ try
 catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
 }
+
+//legacy mysql_connect for non-released code:
+
+$link;
+if (!($link = @mysql_connect($cfg['db_host'], $cfg['db_user'], $cfg['db_pass'] )))   //
+{
+    echo "<BR><BR><CENTER>Connection not possible</CENTER><BR><BR>";
+    @mysql_close($link);
+    exit();
+}
+
+if (!(@mysql_select_db ($cfg['db_name'],$link)))
+{
+    echo "<BR><BR><CENTER>Database access not possible</CENTER><BR><BR>";
+    @mysql_close($link);
+    exit();
+}
+// ***************************************************************************
